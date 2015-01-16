8ea8dab81f84eef99695c6d94179fee0d6401662
agprog <agprog@agprog.net>
2015-01-16 11:10:30 +0100

    maj style

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ module.exports = {
 	MAIL_SERVICE:'free',
 	MAIL_USE_TLS:true,
 	MAIL_HOST:'smtp.free.fr',
-	MAIL_USER:'tn.melin@free.fr',
+	MAIL_USER:'tn.melin',
 	MAIL_PASS:'12k6epmail',
 	MAIL_PORT:587
 	};
